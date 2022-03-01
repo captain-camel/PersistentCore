1258c588962e52e5b420b12c78d733c1fcaf14f1
Cameron Delong <cameron.delong@icloud.com>
2022-03-01 11:53:44 -0700

    Add documentation to `Fetch` extensions

--- Sources/PersistentCore/Fetch/Fetch+or.swift
+++ Sources/PersistentCore/Fetch/Fetch+or.swift
@@ -1,67 +1,124 @@
 //
-//  Fetch+or.swift
+//  File.swift
 //  
 //
-//  Created by Cameron Delong on 2/18/22.
+//  Created by Cameron Delong on 2/24/22.
 //
 
 import CoreData
+import SwiftUI
 
 extension Fetch {
-    func or<Property>(_ keyPath: KeyPath<Object, PersistentObject.Stored<Property>>, equals value: Property) -> Fetch {
-        let key = Object.meta[keyPath: keyPath].unwrappedKey
-        let predicate: NSPredicate
-
-        switch value.storablePrimitive {
-        case nil as Any?:
-            predicate = NSPredicate(format: "\(key) == nil")
-        default:
-            predicate = NSPredicate(format: "\(key) == %@", argumentArray: [value.storablePrimitive])
-        }
-
-        addPredicate(predicate, combinationType: .and)
+    /// Includes objects in fetch if a property is equal to a value.
+    ///
+    /// - Parameters:
+    ///   - keyPath: The property that is being compared. Must be a `KeyPath` to the projected value of a ``Property/Stored`` property, not to the property itself.
+    ///   - value: The value to compare against the property.
+    ///
+    /// - Returns: The ``Fetch`` with the added filter.
+    public func or<PropertyType>(_ keyPath: KeyPath<Object, Property<Object>.Stored<PropertyType>>, equals value: PropertyType) -> Fetch {
+        addPredicate(predicate(
+            key: Object.meta[keyPath: keyPath].unwrappedKey,
+            operation: .equals,
+            value: value.storablePrimitive
+        ), combinationType: .or)
         return self
     }
     
-    func or<Property>(_ keyPath: KeyPath<Object, PersistentObject.Relationship<Property>>, equals value: Property) -> Fetch {
-        let key = Object.meta[keyPath: keyPath].unwrappedKey
-
-        addPredicate(NSPredicate(format: "\(key) == %@", argumentArray: [value.relationshipPrimitive]), combinationType: .and)
+    /// Includes objects in fetch if a property is equal to a value.
+    ///
+    /// - Parameters:
+    ///   - keyPath: The property that is being compared. Must be a `KeyPath` to the projected value of a ``Property/Relationship`` property, not to the property itself.
+    ///   - value: The value to compare against the property.
+    ///
+    /// - Returns: The ``Fetch`` with the added filter.
+    public func or<PropertyType>(_ keyPath: KeyPath<Object, Property<Object>.Relationship<PropertyType>>, equals value: PropertyType) -> Fetch {
+        addPredicate(predicate(
+            key: Object.meta[keyPath: keyPath].unwrappedKey,
+            operation: .equals,
+            value: value.relationshipPrimitive
+        ), combinationType: .or)
         return self
     }
     
-    func or(_ isIncluded: @escaping (Object) -> Bool) -> Fetch {
+    /// Includes objects in fetch if a closure evaluates to `true`.
+    ///
+    /// - Important: Since this method required fetching every object from the data stack, it has significantly worse performance than other filter methods. Avoid using it if possible.
+    ///
+    /// - Parameter isIncluded: A predicate that returns `true` if the object passed as an argument should be included.
+    ///
+    /// - Returns: The ``Fetch`` with the added filter.
+    public func or(_ isIncluded: @escaping (Object) -> Bool) -> Fetch {
         addPredicate(NSPredicate { object, _ in
             return isIncluded(object as! Object)
-        }, combinationType: .and)
+        }, combinationType: .or)
         return self
     }
     
-    func or<Property>(_ keyPath: KeyPath<Object, PersistentObject.Stored<Property>>, greaterThan value: Property) -> Fetch {
-        let key = Object.meta[keyPath: keyPath].unwrappedKey
-
-        addPredicate(NSPredicate(format: "\(key) > %@", argumentArray: [value.storablePrimitive]), combinationType: .and)
+    /// Includes objects in fetch if a property is greater than a value.
+    ///
+    /// - Parameters:
+    ///   - keyPath: The property that is being compared. Must be a `KeyPath` to the projected value of a ``Property/Stored`` property, not to the property itself. Must point to a property that conforms to `Comparable`.
+    ///   - value: The value to compare against the property.
+    ///
+    /// - Returns: The ``Fetch`` with the added filter.
+    public func or<PropertyType: Comparable>(_ keyPath: KeyPath<Object, Property<Object>.Stored<PropertyType>>, greaterThan value: PropertyType) -> Fetch {
+        addPredicate(predicate(
+            key: Object.meta[keyPath: keyPath].unwrappedKey,
+            operation: .greater,
+            value: value.storablePrimitive
+        ), combinationType: .or)
         return self
     }
     
-    func or<Property>(_ keyPath: KeyPath<Object, PersistentObject.Stored<Property>>, lessThan value: Property) -> Fetch {
-        let key = Object.meta[keyPath: keyPath].unwrappedKey
-
-        addPredicate(NSPredicate(format: "\(key) < %@", argumentArray: [value.storablePrimitive]), combinationType: .and)
+    /// Includes objects in fetch if a property is greater than or equal to a value.
+    ///
+    /// - Parameters:
+    ///   - keyPath: The property that is being compared. Must be a `KeyPath` to the projected value of a ``Property/Stored`` property, not to the property itself. Must point to a property that conforms to
+    ///   `Comparable`.
+    ///   - value: The value to compare against the property.
+    ///
+    /// - Returns: The ``Fetch`` with the added filter.
+    public func or<PropertyType: Comparable>(_ keyPath: KeyPath<Object, Property<Object>.Stored<PropertyType>>, greaterThanOrEqualTo value: PropertyType) -> Fetch {
+        addPredicate(predicate(
+            key: Object.meta[keyPath: keyPath].unwrappedKey,
+            operation: .greaterEqual,
+            value: value.storablePrimitive
+        ), combinationType: .or)
         return self
     }
     
-    func or<Property>(_ keyPath: KeyPath<Object, PersistentObject.Stored<Property>>, greaterThanOrEqualTo value: Property) -> Fetch {
-        let key = Object.meta[keyPath: keyPath].unwrappedKey
-
-        addPredicate(NSPredicate(format: "\(key) >= %@", argumentArray: [value.storablePrimitive]), combinationType: .and)
+    /// Includes objects in fetch if a property is less than a value.
+    ///
+    /// - Parameters:
+    ///   - keyPath: The property that is being compared. Must be a `KeyPath` to the projected value of a ``Property/Stored`` property, not to the property itself. Must point to a property that conforms to
+    ///   `Comparable`.
+    ///   - value: The value to compare against the property.
+    ///
+    /// - Returns: The ``Fetch`` with the added filter.
+    public func or<PropertyType: Comparable>(_ keyPath: KeyPath<Object, Property<Object>.Stored<PropertyType>>, lessThan value: PropertyType) -> Fetch {
+        addPredicate(predicate(
+            key: Object.meta[keyPath: keyPath].unwrappedKey,
+            operation: .less,
+            value: value.storablePrimitive
+        ), combinationType: .or)
         return self
     }
     
-    func or<Property>(_ keyPath: KeyPath<Object, PersistentObject.Stored<Property>>, lessThanOrEqualTo value: Property) -> Fetch {
-        let key = Object.meta[keyPath: keyPath].unwrappedKey
-
-        addPredicate(NSPredicate(format: "\(key) <= %@", argumentArray: [value.storablePrimitive]), combinationType: .and)
+    /// Includes objects in fetch if a property is less than or equal to value.
+    ///
+    /// - Parameters:
+    ///   - keyPath: The property that is being compared. Must be a `KeyPath`  to the projected value of a ``Property/Stored`` property, not to the property itself. Must point to a property that conforms to
+    ///   `Comparable`.
+    ///   - value: The value to compare against the property.
+    ///
+    /// - Returns: The ``Fetch`` with the added filter.
+    public func or<PropertyType: Comparable>(_ keyPath: KeyPath<Object, Property<Object>.Stored<PropertyType>>, lessThanOrEqualTo value: PropertyType) -> Fetch {
+        addPredicate(predicate(
+            key: Object.meta[keyPath: keyPath].unwrappedKey,
+            operation: .lessEqual,
+            value: value.storablePrimitive
+        ), combinationType: .or)
         return self
     }
 }
--- Sources/PersistentCore/Fetch/Fetch+sorted.swift
+++ Sources/PersistentCore/Fetch/Fetch+sorted.swift
@@ -1,14 +1,21 @@
 //
-//  Fetch+sorted.swift
+//  File.swift
 //  
 //
-//  Created by Cameron Delong on 2/18/22.
+//  Created by Cameron Delong on 2/24/22.
 //
 
 import CoreData
 
 extension Fetch {
-    func sorted<Property>(by keyPath: KeyPath<Object, PersistentObject.Stored<Property>>, _ order: SortOrder = .ascending) -> Fetch {
+    /// Sorts objects returned by fetch by a property.
+    ///
+    /// - Parameters:
+    ///   - keyPath: The property to use for sorting, which must conform to `Comparable`.
+    ///   - order: The ``SortOrder`` to sort objects into. Either ``SortOrder/ascending`` or ``SortOrder/descending``.
+    ///
+    /// - Returns: The updated ``Fetch``.
+    public func sorted<PropertyType: Comparable>(by keyPath: KeyPath<Object, Property<Object>.Stored<PropertyType>>, _ order: SortOrder = .ascending) -> Fetch {
         if fetchRequest.sortDescriptors == nil {
             fetchRequest.sortDescriptors = []
         }
@@ -17,9 +24,16 @@ extension Fetch {
         return self
     }
     
-    func sorted<Property>(by keyPath: KeyPath<Object, PersistentObject.Stored<Property>>, _ comparator: @escaping (Property, Property) -> ComparisonResult) {
+    /// Sorts objects returned by fetch by a property using a comparator.
+    ///
+    /// - Parameters:
+    ///   - keyPath: The property to use for sorting, which must conform to `Comparable`.
+    ///   - comparator: A closure that returns the comparison result of the arguments.
+    ///
+    /// - Returns: The updated ``Fetch``.
+    public func sorted<PropertyType: Comparable>(by keyPath: KeyPath<Object, Property<Object>.Stored<PropertyType>>, _ comparator: @escaping (PropertyType, PropertyType) -> ComparisonResult) {
         fetchRequest.sortDescriptors?.append(NSSortDescriptor(keyPath: keyPath, ascending: true, comparator: { first, second in
-            switch comparator(first as! Property, second as! Property) {
+            switch comparator(first as! PropertyType, second as! PropertyType) {
             case .ascending:
                 return .orderedAscending
             case .descending:
@@ -30,14 +44,21 @@ extension Fetch {
         }))
     }
     
-    enum ComparisonResult {
+    /// Describes the order of two values.
+    public enum ComparisonResult {
+        /// The first value is less than the second value.
         case ascending
+        /// The second value is less than the second value.
         case descending
+        /// The values are equal.
         case equal
     }
     
-    enum SortOrder {
+    /// The order in which values can be sorted.
+    public enum SortOrder {
+        /// Each successive value is greater than or equal to the last.
         case ascending
+        /// Each successive value is less tahn or equal to the last
         case descending
     }
 }
